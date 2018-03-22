edc567858b8c47b5e8be37cc90ec58afcd922ebf
tlx6550 <912730337@qq.com>
2018-03-22 17:26:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Webpack + Bootstrap 4 Setup (v1.6.2) #
+
+Starter pack for ease of mind
+
+## Current Bootstrap version ##
+
+*4.0.0*
+
+## Build requirements ##
+
+- [Node.js](https://nodejs.org/en/download/)
+
+## How to use ##
+
+`npm install`
+installs dependencies
+
+`npm start`
+starts and opens HMR server for development (localhost:2222)
+
+`npm run build`
+runs build tasks for staging/production (emmits files to `/public/` folder)
---
+++ webpack.config.js
@@ -0,0 +1,129 @@
+// Variables
+
+const isBuild = process.env.NODE_ENV === 'production'
+console.log('isBuild=' + isBuild)
+const webpack = require('webpack')
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const providePluginList = {
+  $: 'jquery',
+  jQuery: 'jquery',
+  'window.jQuery': 'jquery',
+  Popper: ['popper.js', 'default'],
+  // Bootstrap scripts
+  Alert: 'exports-loader?Alert!bootstrap/js/src/alert',
+  Button: 'exports-loader?Button!bootstrap/js/src/button',
+  Carousel: 'exports-loader?Carousel!bootstrap/js/src/carousel',
+  Collapse: 'exports-loader?Collapse!bootstrap/js/src/collapse',
+  Dropdown: 'exports-loader?Dropdown!bootstrap/js/src/dropdown',
+  Modal: 'exports-loader?Modal!bootstrap/js/src/modal',
+  Popover: 'exports-loader?Popover!bootstrap/js/src/popover',
+  Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/src/scrollspy',
+  Tab: 'exports-loader?Tab!bootstrap/js/src/tab',
+  Tooltip: 'exports-loader?Tooltip!bootstrap/js/src/tooltip',
+  Util: 'exports-loader?Util!bootstrap/js/src/util'
+}
+
+// Main config
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [{
+      // SCSS
+      test: /\.scss$/,
+      use: isBuild ? ExtractTextPlugin.extract({
+        publicPath: '',
+        fallback: 'style-loader?sourceMap',
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              minimize: {
+                discardComments: {
+                  removeAll: true
+                }
+              }
+            }
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              ident: 'postcss',
+              plugins: (loader) => [
+                require('autoprefixer')()
+              ]
+            }
+          },
+          'sass-loader?sourceMap'
+        ]
+      }) : [
+        'style-loader?sourceMap',
+        'css-loader?sourceMap',
+        'sass-loader?sourceMap'
+      ]
+    }, {
+      // JS
+      test: /\.js$/,
+      exclude: /.*node_modules\/((?!bootstrap\/js\/src).)*$/,
+      use: isBuild ? [
+        'babel-loader',
+        'eslint-loader'
+      ] : [
+        'babel-loader',
+        'webpack-module-hot-accept',
+        'eslint-loader'
+      ]
+    }, {
+      // HTML
+      test: /\.html$/,
+      use: [
+        'file-loader?name=[path][name].[ext]&context=' + path.resolve(__dirname, 'src'),
+        'extract-loader',
+        'html-loader'
+      ]
+    }, {
+      // Other files
+      test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192
+          }
+        }
+      ]
+    }
+    ]
+  },
+  devServer: {
+    clientLogLevel: 'warning',
+    historyApiFallback: false,
+    hot: true,
+    compress: true,
+    contentBase: path.resolve(__dirname, 'public'),
+    overlay: true,
+    port: 8080
+  },
+  devtool: isBuild ? 'source-map' : 'eval',
+  plugins: isBuild ? [
+    new CleanWebpackPlugin('public'),
+    new webpack.ProvidePlugin(providePluginList),
+    new ExtractTextPlugin('main.css'),
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false,
+      sourceMap: true
+    })
+  ] : [
+    new CleanWebpackPlugin('public'),
+    new webpack.ProvidePlugin(providePluginList),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
+}
